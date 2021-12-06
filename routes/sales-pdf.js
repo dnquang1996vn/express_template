b63f5b6c28d0035aacc63c3fae8a4beffb9c8983
Quinn Do (WebProvise) <quinn.d@webprovise.com>
2021-12-06 17:35:56 +0700

    generate PDF

--- routes/sales-pdf.js
+++ routes/sales-pdf.js
@@ -4,7 +4,9 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/report', function (req, res, next) {
-  exportPdfFromUrl('http://localhost:3000/sales/report').then((pdfData) => {
+  exportPdfFromUrl(
+    req.protocol + '://' + req.headers.host + '/sales/report'
+  ).then((pdfData) => {
     res.contentType('application/pdf');
     res.send(pdfData);
   });
